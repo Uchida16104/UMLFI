a6e02bb9f2c748b628b4b4f1b4b444aeb45a5424
Hirotoshi Uchida <uho02741358@gmail.com>
2026-02-11 12:13:41 +0900

    Update layout.tsx

--- frontend/pages/layout.tsx
+++ frontend/pages/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <script src="https://unpkg.com/htmx.org@1.9.10" defer></script>
-        <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+        <script src="https://unpkg.com/alpinejs@3.13.5/dist/cdn.min.js" defer></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.12" defer></script>
       </head>
       <body className="bg-black antialiased overflow-hidden">
